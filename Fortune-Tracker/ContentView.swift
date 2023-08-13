218bfce152fdbcc5eba4e5228aa4e608cf26c9f0
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-08-13 21:01:10 +0800

    added navigation bar and tab view

--- Fortune-Tracker/ContentView.swift
+++ Fortune-Tracker/ContentView.swift
@@ -9,80 +9,92 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack {
+            NavigationView {
+                TabBarView()
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button(action: {
+                                print("Add button was tapped")
+                            }) {
+                                Image(systemName: "barcode.viewfinder")
+                            }
+                        }
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                print("Settings button was tapped")
+                            }) {
+                                Image(systemName: "line.3.horizontal")
+                            }
+                        }
                     }
-                }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .accentColor(.orange)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            TransactionsView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                }
+            AddView()
+                .tabItem {
+                    Image(systemName: "plus")
+                }
+            AnalysisView()
+                .tabItem {
+                    Image(systemName: "chart.bar.xaxis")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                }
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home Tab Content")
+    }
+}
+
+struct TransactionsView: View {
+    var body: some View {
+        Text("Transactions Tab Content")
+    }
+}
+
+struct AddView: View {
+    var body: some View {
+        Text("Add Tab Content")
+    }
+}
+
+struct AnalysisView: View {
+    var body: some View {
+        Text("Analysis Tab Content")
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Tab Content")
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
